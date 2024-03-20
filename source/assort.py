da99ae2ce253f1457280988bf910a643b048618c
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-03-20 08:44:19 +0000

    test `assort.py`

--- source/assort.py
+++ source/assort.py
@@ -11,50 +11,47 @@ from github import Github
 # import quarkdown
 
 
-## setup
-git = Github(auth = Auth.Token(os.getenv("AQ")))
-repo = git.get_repo("Sup2point0/Assort")
-content = repo.get_contents("")
+with Github(auth = Auth.Token(os.getenv("AQ"))) as git:
 
+  ## setup
+  repo = git.get_repo("Sup2point0/Assort")
+  content = repo.get_contents("")
 
-## extract, sort
-docs = []
-files = []
-
-while content:
-  file = content.pop(0)
-
-  if file.type == "dir":
-    if not file.path.split("/")[-1].startswith("."):
-      folder = repo.get_contents(file.path)
-      content.extend(folder)
-
-  elif file.path.lower().endswith(".md"):  ## NOTE lower needed?
-    files.append(file)
-
-  elif file.path.startswith("docs/"):
-    docs.append(file.path)
-
-
-## render, upload, commit
-for file in files:
-  text = base64.b64decode(file.content)
-  print(text)
-  # file = quarkdown.textualise(text)
-  # path = f"docs/{file.path}.html"
-
-  ## NOTE testing
-  path = f"docs/test.txt"
-  repo.create_file(path, "auto-assort", text)
-  continue
-  ##
-
-  try:
-    existing = repo.get_contents(path)
-    repo.update_file(path, "auto-assort", file.content, sha = existing.sha)
-
-  except:
-    repo.create_file(path, "auto-assort", file.content)
-
-
-git.close()
+  docs = []
+  files = []
+  
+  ## extract
+  while content:
+    file = content.pop(0)
+  
+    if file.type == "dir":
+      if not file.path.split("/")[-1].startswith("."):
+        folder = repo.get_contents(file.path)
+        content.extend(folder)
+  
+    elif file.path.lower().endswith(".md"):  ## NOTE lower needed?
+      files.append(file)
+  
+    elif file.path.startswith("docs/"):
+      docs.append(file.path)
+  
+  ## render, upload
+  for file in files:
+    text = base64.b64decode(file.content)
+    print(text)
+    # file = quarkdown.textualise(text)
+    # path = f"docs/{file.path}.html"
+  
+    ## NOTE testing
+    path = f"docs/test.txt"
+    repo.create_file(path, "auto-assort", text)
+    continue
+    ##
+  
+    ## FIXME?
+    try:
+      existing = repo.get_contents(path)
+      repo.update_file(path, "auto-assort", file.content, sha = existing.sha)
+  
+    except:
+      repo.create_file(path, "auto-assort", file.content)
