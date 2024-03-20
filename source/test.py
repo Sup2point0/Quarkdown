9623279c73c6e5bf5a6abfefee07b5d87fa6cd9c
Sup#2.0 <sup2point0@gmail.com>
2024-03-20 18:22:33 +0000

    test API responses

--- source/test.py
+++ source/test.py
@@ -2,11 +2,12 @@
 Script for GitHub Actions that deploys Markdown files in Assort to GitHub Pages.
 '''
 
+import base64
 import os
 
 from github import Github, Auth
 
-from source import deploy
+import deploy
 
 
 with Github(auth = Auth.Token(os.getenv("AQ"))) as git:
@@ -14,4 +15,5 @@ with Github(auth = Auth.Token(os.getenv("AQ"))) as git:
   ## setup
   repo = git.get_repo("Sup2point0/Assort")
   files = deploy.extract_repo_files(repo)
-  deploy.export_and_deploy(repo, files, "auto-assort")
+  test = repo.get_contents("docs/nonexistent.txt")
+  raise NotImplementedError(test)
