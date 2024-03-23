e36f4bca8d175462a085083e21a40b1199189efb
Sup#2.0 <sup2point0@gmail.com>
2024-03-23 21:31:17 +0000

    update `Context`

--- source/context.py
+++ source/context.py
@@ -5,15 +5,20 @@ class Context:
     '''Convert a `dict` into a `Context` object.'''
 
     self.shard = ctx["opens-ctx"]
-    self.kind = self.shard[:self.shard.index(".")]
+    # self.~ = self.shard[:self.shard.index(".")]
+    self.kind = ctx["kind"]
 
     self.clashes = ctx.get("ctx-clashes", [])
     self.persists = ctx.get("ctx-persists", False)
 
     self.autocloses = (ctx.get("regex.close") == "#AUTO")
+    self.collapses = ctx.get("ctx-collapses", 0)
 
   def done(self) -> bool:
     '''Check if context should be deactivated (when deactivation requisites are met).'''
 
-    if self.autocloses or not self.persists:
+    if self.kind == "#FLAG":
       return True
+
+    # if self.autocloses or not self.persists:
+    #   return True
