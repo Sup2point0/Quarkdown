b781b8558fabb26f4c67b6b9a8f6e183bce808b2
Sup#2.0 <sup2point0@gmail.com>
2024-03-27 21:02:04 +0000

    update deployment engine to log all runs

--- quarkdown/deploy.py
+++ quarkdown/deploy.py
@@ -14,11 +14,21 @@ from github.GithubException import UnknownObjectException
 from . import quarkify
 
 
-__all__ = ["extract_repo_files", "export_and_deploy", "update_logs"]
+EPOCH_OFFSET = 1710000000
+'''Offset to reduce Unix timestamp by for easier management.'''
+
+
+def prepare_deploy(git: Github, repo_name: str) -> tuple[Repository, Repository]:
+  '''Prepares the deployment process by accessing the home and target repositories.'''
+
+  return (
+    git.get_repo("Sup2point0/Quarkdown"),
+    git.get_repo(repo_name),
+  )
 
 
 def extract_repo_files(repo: Repository, path = "") -> list[ContentFile]:
-  '''Extract all .md files from a GitHub repository.'''
+  '''Extract all .md files from the `path` directory of a repository.'''
 
   out = []
   content = repo.get_contents(path)
@@ -37,8 +47,18 @@ def extract_repo_files(repo: Repository, path = "") -> list[ContentFile]:
   return out
 
 
+def extract_repo_json(repo: Repository, path = "") -> dict | list:
+  '''Extract a specific .json file from a GitHub repository.'''
+
+  file = repo.get_contents(path)
+  text = base64.b64decode(file.content)
+  data = json.loads(text)
+
+  return data
+
+
 def export_and_deploy(
-  git: Github,
+  home: Repository,
   repo: Repository,
   files: list[ContentFile],
   *,
@@ -46,37 +66,63 @@ def export_and_deploy(
 ) -> dict:
   '''Export .md files to HTML and commit them to the `docs/` folder of a given GitHub repository. Returns a `dict` log of changed files.'''
 
-  log = extract_logs(git, repo.name.lower())
+  log_home = extract_repo_json(home, f"quarkdown/logs/log.json")
+  log_repo = extract_repo_json(home, f"quarkdown/logs/{repo.name.lower()}.json")
+
+  log_home.insert(0, {
+    "run": len(log_repo), 
+    "epox": round(time.time() - EPOCH_OFFSET),
+    "changes": 0,
+    "data": [],
+  })
 
   for file in files:
-    if not has_changed(file, log):
+    if not has_changed(file, log_repo):
       continue
     
     text = base64.b64decode(file.content).decode()
 
     try:
       export = quarkify.export(text)
-      print(f"export = {export}")
+      path = export["path"]
     except quarkify.Quarkless:
       continue
-
-    content = export["content"]
-    path = export["path"]
 
     try:
       existing = repo.get_contents(path)
     except UnknownObjectException:
-      repo.create_file(path, commit, content)
+      repo.create_file(path, commit, export["content"])
     else:
-      repo.update_file(path, commit, content, existing.sha)
+      repo.update_file(path, commit, export["content"], existing.sha)
 
-    # reduce Unix timestamp for easier management
-    log[file.path.lower()] = {
+    log_home[0]["changes"] += 1
+    log_home[0]["data"].append({"path": file.path, **export})
+
+    log_repo[file.path.lower()] = {
       "export-path": path,
-      "last-export": round(time.time() % 1710000000),
+      "last-export": round(time.time() - EPOCH_OFFSET),
     }
 
-  return log
+  return log_home, log_repo
+
+
+def update_logs(
+  home: Repository,
+  path: str,
+  data: dict,
+  *,
+  commit: str = None,
+):
+  '''Update a .json log file in the Quarkdown repository.'''
+
+  existing = home.get_contents(f"quarkdown/logs/{path}.json")
+
+  home.update_file(
+    existing.path,
+    message = commit or f"#QUARK update logs for {path}",
+    content = json.dumps(data, indent = 2),
+    sha = existing.sha,
+  )
 
 
 def has_changed(file: ContentFile, log: dict) -> bool:
@@ -86,38 +132,7 @@ def has_changed(file: ContentFile, log: dict) -> bool:
   if not deployed:
     return True
 
-  modified = round(file.last_modified_datetime.timestamp() % 1710000000)
+  modified = round(file.last_modified_datetime.timestamp() - EPOCH_OFFSET)
 
   print(f"file {file.name} -> modified: {modified}, deployed: {deployed}")
   return modified - deployed > 0
-
-
-def extract_logs(
-  git: Github,
-  repo_name: str,
-) -> dict:
-  '''Extract logs for a particular repository.'''
-
-  repo = git.get_repo("Sup2point0/Quarkdown")
-  file = repo.get_contents(f"quarkdown/logs/{repo_name.lower()}.json")
-  text = base64.b64decode(file.content)
-
-  return json.loads(text)
-
-
-def update_logs(
-  git: Github,
-  repo_name: str,
-  log: dict,
-) -> None:
-  '''Update logs for a particular repository.'''
-
-  repo = git.get_repo("Sup2point0/Quarkdown")
-  existing = repo.get_contents(f"quarkdown/logs/{repo_name.lower()}.json")
-
-  repo.update_file(
-    path = existing.path,
-    message = f"#QUARK update logs for {repo_name}",
-    content = json.dumps(log, indent = 2),
-    sha = existing.sha,
-  )
--- scripts/assort.py
+++ scripts/assort.py
@@ -4,7 +4,6 @@ Script for GitHub Actions that deploys Markdown files in Assort to GitHub Pages.
 
 import os
 
-
 from github import Github, Auth
 
 # this is ugly, but it'll have to do until we find a workaround
@@ -19,8 +18,10 @@ if key is None:
   raise ValueError("no access key found!")
 
 with Github(auth = Auth.Token(key)) as git:
-  repo = git.get_repo("Sup2point0/Assort")
+  home, repo = qk.prepare_deploy("Assort")
+
   files = qk.extract_repo_files(repo)
-  log = qk.export_and_deploy(git, repo, files, commit = "auto-assort")
-  print(log)
-  qk.update_logs(git, "Assort", log)
+  log_home, log_repo = qk.export_and_deploy(git, repo, files, commit = "auto-assort")
+
+  qk.update_logs(home, "logs", log_home)
+  qk.update_logs(home, "assort", log_repo)
