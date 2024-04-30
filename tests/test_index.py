86bf582586e942e6d8bbdfbf3b11578402d68a75
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-30 10:35:48 +0000

    add index page handling tests

---
+++ tests/test_index.py
@@ -0,0 +1,17 @@
+'''
+Test index page exporting.
+'''
+
+# we really need to find a proper way to do this
+import sys
+sys.path[0] = "/".join(sys.path[0].split("/")[:-1])
+
+from quarkdown import quarkify
+
+
+def test_positive():
+  result = quarkify.extract_quarks('''
+    <!-- #QUARK live! index! -->
+  ''')
+
+  assert result["index"] is True, "index page not detected!"
