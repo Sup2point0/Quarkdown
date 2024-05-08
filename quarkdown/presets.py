98d7f4af41fe936001fe758b3cf04baf7b6d8d35
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-05-08 10:17:06 +0100

    add font presets

--- quarkdown/presets.py
+++ quarkdown/presets.py
@@ -3,13 +3,19 @@ Static resources.
 '''
 
 class url:
+  google_fonts = "https://fonts.googleapis.com/css2?"
   styles = "https://raw.githack.com/Sup2point0/Quarkdown/main/quarkdown/resources/styles"
 
 class css:
+  def font(fonts: list[str]) -> str:
+    '''Format the HTML `<link>` tag for loading fonts from Google Fonts.'''
+
+    return f'''<link rel="stylesheet" type="text/css" href="{url.google_fonts}{"&".join(font for font in fonts)}&display=swap">'''
+    
   def style(style: str) -> str:
     '''Format the HTML `<link>` tag for a given stylesheet.'''
 
-    return f'''<link rel="stylesheet" type="text/css href="{url.styles}/{style.lower()}">'''
+    return f'''<link rel="stylesheet" type="text/css" href="{url.styles}/{style.lower()}">'''
 
 dec_index = {
   "spring": 0.5,
